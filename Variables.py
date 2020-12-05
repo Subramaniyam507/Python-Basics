5884a0fb2a9ab9dd7342bac238096825719ab5f1
subramaniyam narayanan <73768418+Subramaniyam507@users.noreply.github.com>
2020-12-05 22:41:46 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,27 @@
+import turtle
+window=turtle.Screen
+print ("learning variables") 
+a=100;
+print(a)
+
+b=100
+print(a+b)
+c="subbu"
+d=" is great"
+print(c+d)
+h=122.440
+i=223.33
+print(h+i)
+j=3
+k=2
+print(j/k)
+print(j%k)
+subbu=turtle.Turtle()
+subbu.forward(100)
+subbu.right(90)
+subbu.forward(100)
+subbu.right(90)
+subbu.forward(100)
+subbu.right(90)
+subbu.forward(100)
+input("press any key to exit")
---
+++ functions.py
@@ -0,0 +1,20 @@
+import turtle
+window=turtle.Screen
+print("learning functions in pythons")
+#defining a function in python
+subbu=turtle.Turtle() #creating an object of turtle  and storing it in 'subbu' variable
+def squareturtle():
+   
+    subbu.forward(100)
+    subbu.right(90)
+    subbu.forward(100)
+    subbu.right(90)
+    subbu.forward(100)
+    subbu.right(90)
+    subbu.forward(100)
+    subbu.forward(300)
+    input("press any key to exit")   
+    # calling functions in python
+squareturtle()
+# using same subbu turtle object
+
