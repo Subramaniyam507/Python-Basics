58b0bde66972170dce7b9fd83fcc955bb9cd3d4c
subramaniyam narayanan <73768418+Subramaniyam507@users.noreply.github.com>
2020-12-06 00:28:12 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,23 @@
+import turtle
+window=turtle.Screen
+
+subbu=turtle.Turtle()
+
+def squareturtle():
+   
+    subbu.forward(100)
+    subbu.right(90)
+    subbu.forward(100)
+    subbu.right(90)
+    subbu.forward(100)
+    subbu.right(90)
+    subbu.forward(100)
+  
+  
+
+for i in range(4):
+    
+     squareturtle()
+     
+input("enter any key")
+   
